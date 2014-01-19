7a41c564375bab8354c308320ff98d2a5603f661
soukoku <yinchun.wang@gmail.com>
2014-01-19 09:12:54 -0500

    Put version info class under namespace.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,14 +3,17 @@
 [assembly: AssemblyCompany("Yin-Chun Wang")]
 [assembly: AssemblyCopyright("Copyright © Yin-Chun Wang 2014")]
 
-[assembly: AssemblyVersion(_ModernWPFVersionString.Release)]
-[assembly: AssemblyFileVersion(_ModernWPFVersionString.Build)]
-[assembly: AssemblyInformationalVersion(_ModernWPFVersionString.Build)]
+[assembly: AssemblyVersion(ModernWPF._ModernWPFVersionString.Release)]
+[assembly: AssemblyFileVersion(ModernWPF._ModernWPFVersionString.Build)]
+[assembly: AssemblyInformationalVersion(ModernWPF._ModernWPFVersionString.Build)]
 
-static class _ModernWPFVersionString
+namespace ModernWPF
 {
-    // keep this same in majors releases
-    public const string Release = "1.0.0.0";
-    // change this for each nuget release
-    public const string Build = "1.1.30";
+    static class _ModernWPFVersionString
+    {
+        // keep this same in majors releases
+        public const string Release = "1.0.0.0";
+        // change this for each nuget release
+        public const string Build = "1.1.30";
+    }
 }
