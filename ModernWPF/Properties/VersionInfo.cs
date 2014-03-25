a8e59e475c5e5e76b0e24cccaa9f759045455006
soukoku <yinchun.wang@gmail.com>
2014-03-24 21:04:59 -0400

    Added date picker style and placeholder for calendar.

--- ModernWPF/Properties/VersionInfo.cs
+++ ModernWPF/Properties/VersionInfo.cs
@@ -6,5 +6,5 @@
 // keep this same in major releases
 [assembly: AssemblyVersion("1.0.0.0")]
 // change this for each nuget release
-[assembly: AssemblyFileVersion("1.1.38")]
-[assembly: AssemblyInformationalVersion("1.1.38")]
+[assembly: AssemblyFileVersion("1.1.40")]
+[assembly: AssemblyInformationalVersion("1.1.40")]
