e898589b049177af2a762cfdd1e10d720c0e2802
soukoku <yinchun.wang@gmail.com>
2014-09-28 22:06:39 -0400

    Context menu workaround for XP and like even though no one should be using it.

---
+++ ModernWPF/PlatformInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ModernWPF
+{
+    /// <summary>
+    /// Some lame properties for workarounds.
+    /// </summary>
+    public static class PlatformInfo
+    {
+        /// <summary>
+        /// Flag for legacy OS that don't display things correctly.
+        /// Mostly for XP and like.
+        /// </summary>
+        public static bool IsLegacyOS
+        {
+            get
+            {
+                return !CommonWin32.PlatformInfo.IsWinVistaUp;
+            }
+        }
+        
+    }
+}
